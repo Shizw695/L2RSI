e012dfcf914a4a932efbbea8b1ef3ae8ce74b644
Shizw695 <75065392+Shizw695@users.noreply.github.com>
2025-03-11 22:32:25 +0800

    Update Qualitative_Place_Recognition_Results.vue

--- src/components/sections/Qualitative_Place_Recognition_Results.vue
+++ src/components/sections/Qualitative_Place_Recognition_Results.vue
@@ -1,1 +1,20 @@
+<script setup>
+import abstract from '../mds/method.mdx';
+</script>
 
+<template>
+  <div>
+    <el-divider />
+
+    <el-row justify="center">
+        <el-col :xs="24" :sm="20" :md="16" :lg="12" :xl="12">
+            <abstract />
+        </el-col>
+    </el-row>
+
+  </div>
+</template>
+
+<style scoped>
+
+</style>
