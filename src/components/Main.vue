7396ea0c539d2eea42bbeabcad7d5e02532209e7
Shizw695 <75065392+Shizw695@users.noreply.github.com>
2025-03-11 22:31:40 +0800

    Update Main.vue

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,7 +1,7 @@
 <script>
 import Title from './sections/Title.vue'
 import Abstract from './sections/Abstract.vue'
-import Method from './sections/BibTeX.vue'
+import Method from './sections/Method.vue'
 import Qualitative_Place_Recognition_Results from './sections/Qualitative_Place_Recognition_Results.vue'
 import BibTeX from './sections/BibTeX.vue'
 
