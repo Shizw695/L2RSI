3983b90bccbc1d7b0eef078fceef086185ac3625
Shizw695 <75065392+Shizw695@users.noreply.github.com>
2025-03-12 11:27:06 +0800

    Update Title.vue

--- src/components/sections/Title.vue
+++ src/components/sections/Title.vue
@@ -115,14 +115,14 @@ const buttons = [
   <div>
 
     <!-- 最新消息提示 -->
-    <el-row justify="center">
+    <!--el-row justify="center">
       <el-col :span="24">
         <el-alert title="🔥 This template is still under development." type="success" />
       </el-col>
     </el-row>
 
     <!-- 文章logo -->
-    <el-row v-if="logo" justify="center">
+    <!--el-row v-if="logo" justify="center">
       <el-image :src="logo" class="logo" fit="cover" />
     </el-row>
 
