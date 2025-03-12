cd19af64db509a25c435bd8278405df16b4afc3b
Shizw695 <75065392+Shizw695@users.noreply.github.com>
2025-03-12 11:17:52 +0800

    Update Qualitative_Place_Recognition_Results.vue

--- src/components/sections/Qualitative_Place_Recognition_Results.vue
+++ src/components/sections/Qualitative_Place_Recognition_Results.vue
@@ -9,6 +9,11 @@ import Qualitative_Place_Recognition_Results from '../mds/Qualitative_Place_Reco
     <el-row justify="center">
         <el-col :xs="24" :sm="20" :md="16" :lg="12" :xl="12">
             <Qualitative_Place_Recognition_Results />
+            <el-container class="video-container">
+              <video controls muted preload playsinline>
+                <source src="/video/video.mp4" type="video/mp4">
+              </video>
+            </el-container>
         </el-col>
     </el-row>
 
