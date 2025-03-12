ba484795212227071d63faee98bd53455b0030d0
Shizw695 <75065392+Shizw695@users.noreply.github.com>
2025-03-12 16:15:37 +0800

    Update Qualitative_Place_Recognition_Results.vue

--- src/components/sections/Qualitative_Place_Recognition_Results.vue
+++ src/components/sections/Qualitative_Place_Recognition_Results.vue
@@ -1,5 +1,4 @@
 <script setup>
-import Qualitative_Place_Recognition_Results from '../mds/Qualitative_Place_Recognition_Results.mdx';
 </script>
 
 <template>
@@ -7,14 +6,33 @@ import Qualitative_Place_Recognition_Results from '../mds/Qualitative_Place_Reco
     <el-divider />
 
     <el-row justify="center">
-        <el-col :xs="24" :sm="20" :md="16" :lg="12" :xl="12">
-            <Qualitative_Place_Recognition_Results />
-        </el-col>
+      <h1 class="section-title">Qualitative Place Recognition Results</h1>
     </el-row>
 
+    <!-- 每个网站的视频的iframe可能不一致，最好在这里手动调整 -->
+    <el-row justify="center">
+      <el-col :xs="24" :sm="20" :md="16" :lg="12" :xl="10" >
+
+        <!-- local -->
+        <el-container class="video-container">
+          <video controls muted preload playsinline>
+            <source src="/video/L2RSI_video_100km2.mp4" type="video/mp4">
+          </video>
+        </el-container>
+      </el-col>
+    </el-row>
   </div>
 </template>
 
 <style scoped>
-  
+
+.video-container{
+  margin: 20px 0px 0px 0px;
+}
+
+iframe, video {
+  aspect-ratio: 16 / 9;
+  width: 100%;
+}
+
 </style>
