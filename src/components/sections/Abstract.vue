c5bd0e607d9f5000de925bba7774a1b9947653c0
Shizw695 <75065392+Shizw695@users.noreply.github.com>
2025-05-24 22:17:37 +0800

    Update Abstract.vue

--- src/components/sections/Abstract.vue
+++ src/components/sections/Abstract.vue
@@ -8,6 +8,12 @@ import abstract from '../mds/abstract.mdx';
 
     <el-row justify="center">
         <el-col :xs="24" :sm="20" :md="16" :lg="12" :xl="12">
+            <img
+              src="@/assets/arxiv_introduction_new.jpg"
+              alt="Introduction"
+              style="width: 100%; display: block; margin: 0 auto 20px auto;"
+            />
+
             <abstract />
         </el-col>
     </el-row>
