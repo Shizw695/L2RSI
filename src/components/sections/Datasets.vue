9615bd2dad4d288b7e731dc800f9669231a1e2da
Shizw695 <75065392+Shizw695@users.noreply.github.com>
2025-10-16 15:47:31 +0800

    Update Datasets.vue

--- src/components/sections/Datasets.vue
+++ src/components/sections/Datasets.vue
@@ -1,1 +1,22 @@
+<script setup>
+import method from '../mds/datasets.mdx';
+</script>
 
+<template>
+  <div>
+    <el-divider />
+
+    <el-row justify="center">
+        <el-col :xs="24" :sm="20" :md="16" :lg="12" :xl="12">
+            <method />
+        </el-col>
+    </el-row>
+
+  </div>
+</template>
+
+<style scoped>
+.el-col {
+  text-align: justify;
+}
+</style>
