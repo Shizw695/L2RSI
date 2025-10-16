349366ff2280dbfe59835235608bbab6467fb85d
Shizw695 <75065392+Shizw695@users.noreply.github.com>
2025-10-16 16:20:54 +0800

    Update Method.vue

--- src/components/sections/Method.vue
+++ src/components/sections/Method.vue
@@ -9,6 +9,11 @@ import method from '../mds/method.mdx';
     <el-row justify="center">
         <el-col :xs="24" :sm="20" :md="16" :lg="12" :xl="12">
             <method />
+            <img
+              src="/public/img_framework_0516.jpg"
+              alt="Introduction"
+              style="width: 100%; display: block; margin: 0 auto 20px auto;"
+            />
         </el-col>
     </el-row>
 
